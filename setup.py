a7b83996efeec6a4d54f6053e25e7e302c90d771
Guillaume Chorn <guillaumechorn@gmail.com>
2018-04-11 22:48:19 -0700

    Add/modify setup files for generation of pypi packages

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='golden_marshmallows',
-    version='0.1.%s' % int(time()),
+    version='0.1',
     author='Guillaume Chorn',
     author_email='guillaume.chorn@gmail.com',
     packages=find_packages(),
+    url='https://github.com/gchorn/golden-marshmallows',
+    download_url='https://github.com/gchorn/golden-marshmallows/archive/v0.1.0.tar.gz',
     description='Marshmallow Schema subclass that auto-defines fields based on'
                 ' SQLAlchemy classes',
     install_requires=['marshmallow>=2.9.0', 'SQLAlchemy>=0.9.8']
