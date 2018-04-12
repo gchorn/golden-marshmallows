91255d1eeff0d0c1fd1e3fefc68873192f8eea92
Guillaume Chorn <guillaumechorn@gmail.com>
2018-04-11 22:59:02 -0700

    Fix version and download url in setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from time import time
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='golden_marshmallows',
-    version='0.1',
+    version='0.1.2',
     author='Guillaume Chorn',
     author_email='guillaume.chorn@gmail.com',
-    packages=find_packages(),
+    packages=['golden_marshmallows'],
     url='https://github.com/gchorn/golden-marshmallows',
-    download_url='https://github.com/gchorn/golden-marshmallows/archive/v0.1.0.tar.gz',
+    download_url='https://github.com/gchorn/golden-marshmallows/archive/v0.1.2.tar.gz',
     description='Marshmallow Schema subclass that auto-defines fields based on'
                 ' SQLAlchemy classes',
     install_requires=['marshmallow>=2.9.0', 'SQLAlchemy>=0.9.8']
