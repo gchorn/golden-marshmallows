333d5f68a52f8141a8aff7054f13b99ff75e7ac5
Guillaume Chorn <guillaume.chorn@zefr.com>
2018-05-11 12:19:09 -0700

    Add long description to setup to display README on pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-from time import time
 from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='golden_marshmallows',
-    version='0.1.2',
+    version='0.2.0',
     author='Guillaume Chorn',
     author_email='guillaume.chorn@gmail.com',
     packages=['golden_marshmallows'],
     url='https://github.com/gchorn/golden-marshmallows',
-    download_url='https://github.com/gchorn/golden-marshmallows/archive/v0.1.2.tar.gz',
+    download_url='https://github.com/gchorn/golden-marshmallows/archive/v0.2.0.tar.gz',
     description='Marshmallow Schema subclass that auto-defines fields based on'
                 ' SQLAlchemy classes',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['marshmallow>=2.9.0', 'SQLAlchemy>=0.9.8']
 )
